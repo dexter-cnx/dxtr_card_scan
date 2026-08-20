c06d4932bc5db81b555fce3b56eb0fd21812dc4e
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-20 18:44:43 +0700

    Test landscape frame clamping

--- test/capture_frame_test.dart
+++ test/capture_frame_test.dart
@@ -12,4 +12,14 @@ void main() {
     expect(rect.width, 800);
     expect(rect.width / rect.height, closeTo(85.60 / 53.98, 0.0001));
   });
+
+  test('ID-1 frame clamps height in a wide landscape viewport', () {
+    const viewport = Size(1000, 400);
+    const frame = CaptureFrame.id1(widthFactor: .88, maxHeightFactor: .8);
+    final rect = frame.resolve(viewport);
+    expect(rect.center, const Offset(500, 200));
+    expect(rect.height, 320);
+    expect(rect.width, lessThanOrEqualTo(viewport.width));
+    expect(rect.width / rect.height, closeTo(85.60 / 53.98, 0.0001));
+  });
 }
