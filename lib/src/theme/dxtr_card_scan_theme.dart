e025175dc9cb15045810079c9d51a0e3ae19cdd1
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-20 21:12:12 +0700

    Theme camera control surfaces

--- lib/src/theme/dxtr_card_scan_theme.dart
+++ lib/src/theme/dxtr_card_scan_theme.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 
 import '../crop/image_crop_style.dart';
 import '../frame/capture_frame_style.dart';
+import 'camera_controls_style.dart';
 
 /// Theme extension shared by Camera capture and Gallery crop surfaces.
 class DxtrCardScanTheme extends ThemeExtension<DxtrCardScanTheme> {
   const DxtrCardScanTheme({
     this.captureFrameStyle = const CaptureFrameStyle(),
     this.imageCropStyle = const ImageCropStyle(),
+    this.cameraControlsStyle = const CameraControlsStyle(),
   });
 
   final CaptureFrameStyle captureFrameStyle;
   final ImageCropStyle imageCropStyle;
+  final CameraControlsStyle cameraControlsStyle;
 
   @override
   DxtrCardScanTheme copyWith({
     CaptureFrameStyle? captureFrameStyle,
     ImageCropStyle? imageCropStyle,
+    CameraControlsStyle? cameraControlsStyle,
   }) {
     return DxtrCardScanTheme(
       captureFrameStyle: captureFrameStyle ?? this.captureFrameStyle,
       imageCropStyle: imageCropStyle ?? this.imageCropStyle,
+      cameraControlsStyle: cameraControlsStyle ?? this.cameraControlsStyle,
     );
   }
 
@@ -39,6 +44,11 @@ class DxtrCardScanTheme extends ThemeExtension<DxtrCardScanTheme> {
       imageCropStyle: ImageCropStyle.lerp(
         imageCropStyle,
         other.imageCropStyle,
+        t,
+      ),
+      cameraControlsStyle: CameraControlsStyle.lerp(
+        cameraControlsStyle,
+        other.cameraControlsStyle,
         t,
       ),
     );
