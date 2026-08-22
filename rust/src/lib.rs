fdd017e0960f09c56b1dc6b08e520d7301921d32
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-22 15:18:44 +0700

    feat: export quadrilateral detection API

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,11 @@
+pub mod detection;
 mod ffi;
 pub mod model;
 pub mod processor;
 
+pub use detection::{
+    detect_card_quad, CandidateScore, DetectionOptions, DetectionResult, Point, Quad,
+};
 pub use ffi::{card_scan_process, card_scan_result_free, CardScanResult};
 pub use model::{NormalizedRect, OutputFormat, ProcessorOptions};
 pub use processor::process_encoded;
