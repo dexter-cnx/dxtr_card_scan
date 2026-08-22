68c9e3eb033934ca510f4aa5ef2bfd7cad251d22
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-22 23:29:37 +0700

    feat(gallery): export native detector ABI

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,7 +7,7 @@ pub mod warp;
 pub use detection::{
     detect_card_quad, CandidateScore, DetectionOptions, DetectionResult, Point, Quad,
 };
-pub use ffi::{card_scan_process, card_scan_result_free, CardScanResult};
+pub use ffi::{card_scan_detect, card_scan_process, card_scan_result_free, CardScanResult};
 pub use model::{NormalizedRect, OutputFormat, ProcessorOptions};
 pub use processor::process_encoded;
 pub use warp::{warp_quad, WarpOptions};
