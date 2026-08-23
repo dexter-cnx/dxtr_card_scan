d88c76fcad3b1501b2bad9ac497891dbe5fe52d1
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-23 16:42:00 +0700

    fix(quality): export quality ABI module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,17 +4,16 @@ mod ffi;
 pub mod model;
 pub mod processor;
 pub mod quality;
+mod quality_ffi;
 pub mod warp;
 
 pub use contrast_region::{detect_card_quad_with_contrast_fallback, detect_contrast_region};
 pub use detection::{
     detect_card_quad, CandidateScore, DetectionOptions, DetectionResult, Point, Quad,
 };
-pub use ffi::{
-    card_scan_analyze_quality, card_scan_detect, card_scan_process, card_scan_result_free,
-    CardScanResult,
-};
+pub use ffi::{card_scan_detect, card_scan_process, card_scan_result_free, CardScanResult};
 pub use model::{NormalizedRect, OutputFormat, ProcessorOptions};
 pub use processor::process_encoded;
 pub use quality::{analyze_quality, BlurMeasurement, ExposureMeasurement, QualityAnalysis};
+pub use quality_ffi::card_scan_analyze_quality;
 pub use warp::{warp_quad, WarpOptions};
